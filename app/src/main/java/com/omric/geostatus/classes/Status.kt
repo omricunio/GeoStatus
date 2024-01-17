33f8f4a6acdf0ca40e89b2fb5a86260fd4f836eb
omricunio <omricunio@gmail.com>
2024-01-17 14:58:00 +0200

    Switched to firestore instead of real time db

--- app/src/main/java/com/omric/geostatus/classes/Status.kt
+++ app/src/main/java/com/omric/geostatus/classes/Status.kt
@@ -1,5 +1,5 @@
 package com.omric.geostatus.classes
 
-class Status(val name: String, val date: String) {
+class Status(val name: String, val date: String, val imagePath: String, val creator: String) {
 
 }
